f912788cdd20219011a0b4dbb822c35d06b67425
Elena Grahovac <elena@grahovac.me>
2017-02-28 22:23:33 +0700

    Update book links

--- parse/parse.go
+++ parse/parse.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"strings"
+	"encoding/json"
 	"encoding/xml"
 	"fmt"
-	"encoding/json"
 	"net/http"
 	"strconv"
+	"strings"
 )
 
 func standardizeSpaces(s string) string {
@@ -16,42 +16,42 @@ func standardizeSpaces(s string) string {
 func main() {
 
 	for i := 0; i < 1000; i++ {
-	path := "http://www.mann-ivanov-ferber.ru/book/all.ajax?booktype=paperbook&page=" + strconv.Itoa(i)
-	//path := "http://www.mann-ivanov-ferber.ru/book/all.ajax?booktype=ebook&page=" + strconv.Itoa(i)
-	//path := "http://www.mann-ivanov-ferber.ru/book/all.ajax?booktype=audiobook&page=" + strconv.Itoa(i)
-	resp, err := http.Get(path)
-	if err != nil {
-		fmt.Println("Error opening file:", err)
-		return
-	}
+		//path := "http://www.mann-ivanov-ferber.ru/book/all.ajax?booktype=paperbook&page=" + strconv.Itoa(i)
+		//path := "http://www.mann-ivanov-ferber.ru/book/all.ajax?booktype=ebook&page=" + strconv.Itoa(i)
+		path := "http://www.mann-ivanov-ferber.ru/book/all.ajax?booktype=audiobook&page=" + strconv.Itoa(i)
+		resp, err := http.Get(path)
+		if err != nil {
+			fmt.Println("Error opening file:", err)
+			return
+		}
 
-	body := struct {
-		Status string `json:"status"`
-		Message string `json:"message"`
-		HTML string `json:"html"`
-	}{}
-	json.NewDecoder(resp.Body).Decode(&body)
+		body := struct {
+			Status  string `json:"status"`
+			Message string `json:"message"`
+			HTML    string `json:"html"`
+		}{}
+		json.NewDecoder(resp.Body).Decode(&body)
 
-	if body.Status != "ok" {
-		return
-	}
+		if body.Status != "ok" {
+			return
+		}
 
-	body.HTML = standardizeSpaces("<body><books>" + body.HTML + "</books></body>")
-	body.HTML = strings.Replace(body.HTML, "&nbsp;", "", -1)
+		body.HTML = standardizeSpaces("<body><books>" + body.HTML + "</books></body>")
+		body.HTML = strings.Replace(body.HTML, "&nbsp;", "", -1)
 
-	type Book struct {
-		Val string `xml:"href,attr"`
-	}
+		type Book struct {
+			Val string `xml:"href,attr"`
+		}
 
-	type Data struct {
-		Books []Book `xml:"books>div>a"`
-	}
+		type Data struct {
+			Books []Book `xml:"books>div>a"`
+		}
 
-	container := Data{}
-	xml.Unmarshal([]byte(body.HTML), &container)
+		container := Data{}
+		xml.Unmarshal([]byte(body.HTML), &container)
 
-	for _, item := range container.Books {
-		fmt.Println(item.Val)
-	}
+		for _, item := range container.Books {
+			fmt.Println("audiobook," + item.Val)
+		}
 	}
 }
