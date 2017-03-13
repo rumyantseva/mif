23194797579a8f17ae87a9bd636762a20732f23b
Elena Grahovac <elena@grahovac.me>
2017-03-13 17:45:02 +0700

    Improve docker preparation

--- make.sh
+++ make.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/env bash
 
 # Prepare & push images to dockerhub.
+VERSION=0.0.2
 
 # Prepare database with dump
-docker build --no-cache=true -t webdeva/mif-postgresql:0.0.2 -f ./db/Dockerfile .
-docker push webdeva/mif-postgresql
+echo "Prepare DB for version "$VERSION
+
+docker build --no-cache=true -t webdeva/mif-postgresql:$VERSION -f ./db/Dockerfile .
+docker push webdeva/mif-postgresql:$VERSION
+
+docker build --no-cache=true -t webdeva/mif-postgresql:latest -f ./db/Dockerfile .
+docker push webdeva/mif-postgresql:latest
 
 # Prepare application
+echo "Prepare app for version "$VERSION
+
 env GOOS=linux GOARCH=amd64 go build .
-docker build --no-cache=true -t webdeva/mif-app:0.0.2 -f ./Dockerfile .
-docker push webdeva/mif-app
+docker build --no-cache=true -t webdeva/mif-app:$VERSION -f ./Dockerfile .
+docker push webdeva/mif-app:$VERSION
+
+docker build --no-cache=true -t webdeva/mif-app:latest -f ./Dockerfile .
+docker push webdeva/mif-app:latest
